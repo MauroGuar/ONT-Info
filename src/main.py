3286d79e56a19b82a1691c1e523133bbad867da7
MauroGuar <m.guarnieri@alumno.etec.um.edu.ar>
2023-10-03 12:36:04 -0300

    Optimizing the SSH connection using a separate file

--- src/main.py
+++ src/main.py
@@ -1,32 +1,19 @@
 import paramiko
+from ssh_connection import ssh_get_connection
 
 hostname = "172.16.17.48"
 username = "mauro"
 port = 22
 password = "admin123"
 
-ssh = paramiko.SSHClient()
+ssh = ssh_get_connection(hostname, port, username, password)
 
-ssh.load_system_host_keys()
+stdin, stdout, stderr = ssh.exec_command("pwd")
 
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+output_lines = stdout.readlines()
 
-try:
-    ssh.connect(hostname=hostname, port=port, username=username, password=password)
+print("Resultado del comando:")
+for line in output_lines:
+    print(line.strip())
 
-    comando = "pwd"
-    stdin, stdout, stderr = ssh.exec_command(comando)
-
-    output_lines = stdout.readlines()
-
-    print("Resultado del comando:")
-    for line in output_lines:
-        print(line.strip())
-
-except paramiko.AuthenticationException:
-    print("Error de autenticación. Por favor, verifica tus credenciales.")
-except paramiko.SSHException as e:
-    print("Error al establecer la conexión SSH:", str(e))
-finally:
-    # Cerrar la conexión SSH
-    ssh.close()
+ssh.close()
