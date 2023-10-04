1152009b78e78a63f19497b410d6f725bb28b300
MauroGuar <m.guarnieri@alumno.etec.um.edu.ar>
2023-10-04 12:18:23 -0300

    Creating branch for pexpect

--- src/main.py
+++ src/main.py
@@ -1,23 +1,11 @@
-from ssh_connection import ssh_get_connection
+import pexpect
 
 
 def main():
-    hostname = "172.16.17.48"
-    username = "mauro"
+    hostname = "172.17.254.45"
+    username = "root"
     port = 22
     password = "admin123"
-
-    ssh = ssh_get_connection(hostname, port, username, password)
-
-    stdin, stdout, stderr = ssh.exec_command("pwd")
-
-    output_lines = stdout.readlines()
-
-    print("Resultado del comando:")
-    for line in output_lines:
-        print(line.strip())
-
-    ssh.close()
 
 
 if __name__ == "__main__":
