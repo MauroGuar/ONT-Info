d6f37b9051cf2d7a0134a0f7247ed063381e82f0
MauroGuar <m.guarnieri@alumno.etec.um.edu.ar>
2023-10-30 18:15:31 -0300

    Changing the function to format the time

--- app/data_processing/ont_info.py
+++ app/data_processing/ont_info.py
@@ -9,13 +9,12 @@ from re import sub
 
 
 # Define a function for formatting the time
-def format_time(date_time, new_query):
+def format_time(date_time):
     """
-    Format the given date and time to a specific time zone (Buenos Aires) and optionally adjust the hour.
+    Format the given date and time to a specific time zone (Buenos Aires).
 
     Args:
         date_time (datetime): The date and time to format.
-        new_query (bool): A flag indicating whether it's a new query.
 
     Returns:
         str: The formatted time in "HH:MM" format.
@@ -26,40 +25,20 @@ def format_time(date_time, new_query):
     # Convert the input date_time to the Buenos Aires time zone
     date_time_with_tz = date_time.astimezone(bsas_timezone)
 
-    # Checks if it is not a new query since the new queries are already date-time formatted
-    if not new_query:
-        # Calculate the time shift and adjust the hour
-        # Extract the time zone offset from the 'date_time_with_tz' and convert it to an integer.
-        time_shift = int(sub(r"0+$", "", date_time_with_tz.strftime("%z")))
-
-        # Get the time in "HH:MM" format from 'date_time_with_tz'.
-        time = date_time_with_tz.strftime("%H:%M")
-
-        # Split the 'time' into hours and minutes, creating a list called 'hours_minutes'.
-        hours_minutes = time.split(":")
-
-        # Calculate the adjusted hour by adding 'time_shift' to the original hour value.
-        hour = int(hours_minutes[0]) + time_shift
-
-        # Create a formatted time string by combining the adjusted hour and the original minutes.
-        time_formatted = str(hour) + ":" + hours_minutes[1]
-
-    else:
-        # Get the time in "HH:MM" format
-        time_formatted = date_time_with_tz.strftime("%H:%M")
+    # Get the time in "HH:MM" format
+    time_formatted = date_time_with_tz.strftime("%H:%M")
 
     return time_formatted
 
 
 # Define a function to convert a query into a dictionary for display
-def convert_query_to_show(query, items_to_show, new_query):
+def convert_query_to_show(query, items_to_show):
     """
     Convert a query result into a dictionary with formatted date, time, and relevant information.
 
     Args:
         query (dict): The query result containing date_time and ont_info.
         items_to_show (list): A list of specific items to display.
-        new_query (bool): A flag indicating whether it's a new query.
 
     Returns:
         str: The formatted date.
@@ -68,7 +47,7 @@ def convert_query_to_show(query, items_to_show, new_query):
     """
     date_time = query["date_time"]
     date = date_time.strftime("%d/%m/%Y")
-    time = format_time(date_time, new_query)
+    time = format_time(date_time)
     dictionary_to_show = {**query["ont_info"][0], **query["ont_info"][1]}
 
     if items_to_show is not None:
@@ -97,14 +76,13 @@ def query_refresh(
         tuple: A tuple containing date (str), time (str), and a dictionary of relevant information.
     """
     query = new_query(olt_ip_introduced, ont_sn_introduced, debug_mode)
-    data_to_show = convert_query_to_show(query, items_to_show, True)
+    data_to_show = convert_query_to_show(query, items_to_show)
     return data_to_show
 
 
 # Define a function for making a new request
 def new_request(
-        olt_ip_introduced, ont_sn_introduced, items_to_show=None, debug_mode=False
-):
+        olt_ip_introduced, ont_sn_introduced, items_to_show=None, debug_mode=False):
     """
     Make a new request for a specific OLT IP and ONT serial number.
 
@@ -122,7 +100,7 @@ def new_request(
     old_query_in_range = find_query_in_range(olt_ip_introduced, ont_sn_introduced)
     if old_query_in_range is None:
         query = new_query(olt_ip_introduced, ont_sn_introduced, debug_mode)
-        data_to_show = convert_query_to_show(query, items_to_show, True)
+        data_to_show = convert_query_to_show(query, items_to_show)
     else:
         data_to_show = convert_query_to_show(old_query_in_range, items_to_show)
 
