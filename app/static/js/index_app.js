af30d05e840ed0078e95383a6e60deb6baaf52ac
MauroGuar <m.guarnieri@alumno.etec.um.edu.ar>
2023-10-31 23:31:19 -0300

    Hide form waiting message when result page is displayed in case the user returns to the page

--- app/static/js/index_app.js
+++ app/static/js/index_app.js
@@ -1,11 +1,38 @@
+/**
+ * @fileoverview This script handles displaying the wait message when the form is submitted.
+ */
+
+/**
+ * @type {HTMLElement}
+ * @description The element that displays the waiting message.
+ */
 const waitMsg = document.getElementById('wait-msg');
+
+/**
+ * @type {HTMLFormElement}
+ * @description The form element.
+ */
 const form = document.getElementById('ip-sn-form');
 
+/**
+ * An event listener that displays the waiting message when the form is submitted.
+ */
 form.addEventListener('submit', function () {
+    /**
+     * Changes the display style of waitMsg to "flex" when the form is submitted.
+     */
     waitMsg.style.display = "flex";
-    localStorage.setItem("submittedForm", "true")
 });
 
-if (localStorage.getItem("submittedForm") === "true") {
-    waitMsg.style.display = "none"
+/**
+ * An event listener that handles the unloading of the window.
+ * This is used so that the waiting message is hidden again in case the user returns to the page.
+ */
+window.onunload = function (e) {
 }
+
+
+
+
+
+
